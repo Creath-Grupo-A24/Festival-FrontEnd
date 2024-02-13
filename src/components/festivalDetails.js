5f9aa9d7194fe32039d0db34755072f9543d936e
gustavofg1pontes <gustavo.andre@aluno.ifsp.edu.br>
2024-02-13 16:03:20 -0300

    style festivaldetails

--- src/components/festivalDetails.js
+++ src/components/festivalDetails.js
@@ -1,41 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { getEventById } from './festivalService'; 
-import { getSubscriptionByEventId } from './eventService';
-import './festivalDetails.css';
-import UploadForm from '../rules/uploadRules';
-import {downloadRules} from './festivalService';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { getEventById } from "./festivalService";
+import { getSubscriptionByEventId } from "./eventService";
+import "./festivalDetails.css";
+import UploadForm from "../rules/uploadRules";
+import { downloadRules } from "./festivalService";
+import moment from "moment";
+import { Helmet } from "react-helmet";
 
 const FestivalDetails = () => {
   let { id } = useParams();
   const [festival, setFestival] = useState(null);
   const [subscriptions, setSubscriptions] = useState([]);
   const navigate = useNavigate();
-  const token = localStorage.getItem('token');
-  const user = localStorage.getItem('user');
+  const token = localStorage.getItem("token");
+  const user = localStorage.getItem("user");
   const userString = user ? JSON.parse(user) : {};
-  const [showUploadForm, setShowUploadForm] = useState(false); 
+  const [showUploadForm, setShowUploadForm] = useState(false);
 
   const handleShowUploadForm = () => {
-      setShowUploadForm(true);
+    setShowUploadForm(true);
   };
 
   const handleDownload = async (id) => {
     try {
-          const response = await downloadRules(id);
-          const url = window.URL.createObjectURL(new Blob([response.data]));
-          const link = document.createElement('a');
-          link.href = url;
-          link.setAttribute('download', 'rules.pdf'); 
-          document.body.appendChild(link);
-          link.click();
-          window.URL.revokeObjectURL(url);
-          link.remove();
-      } catch (error) {
-          console.error('Erro ao baixar as regras:', error);
-      }
+      const response = await downloadRules(id);
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "rules.pdf");
+      document.body.appendChild(link);
+      link.click();
+      window.URL.revokeObjectURL(url);
+      link.remove();
+    } catch (error) {
+      console.error("Erro ao baixar as regras:", error);
+    }
   };
-
 
   useEffect(() => {
     const fetchFestivalDetails = async () => {
@@ -43,7 +44,7 @@ const FestivalDetails = () => {
         const fetchedFestival = await getEventById(id);
         setFestival(fetchedFestival);
       } catch (error) {
-        console.error('Erro ao buscar detalhes do evento', error);
+        console.error("Erro ao buscar detalhes do evento", error);
       }
     };
 
@@ -56,64 +57,110 @@ const FestivalDetails = () => {
       console.log(response.data.items);
       setSubscriptions(response.data.items);
     } catch (error) {
-      console.error('Erro ao buscar inscrições:', error);
+      console.error("Erro ao buscar inscrições:", error);
     }
   };
 
   const redirectToInscriptionPage = () => {
-    console.log("Roles",user);
+    console.log("Roles", user);
     if (user) {
       const userString = user ? JSON.parse(user) : null;
-      if (userString && userString.roles && (userString.roles.includes('MANAGER') || userString.roles.includes('DANCER'))){
+      if (
+        userString &&
+        userString.roles &&
+        (userString.roles.includes("MANAGER") ||
+          userString.roles.includes("DANCER"))
+      ) {
         console.log(token);
         navigate(`/inscricao/${id}`);
-      }
-      else{
-        alert('Apenas gerentes e dançarinos podem realizar inscrição, voce sera redirecionado para a pagina inicial!');
-        navigate('/');
+      } else {
+        alert(
+          "Apenas gerentes e dançarinos podem realizar inscrição, voce sera redirecionado para a pagina inicial!"
+        );
+        navigate("/");
       }
     } else {
-      alert('Faça o login antes de continuar, você sera enviado para a pagina de login');
-      navigate('/loginusuario');
+      alert(
+        "Faça o login antes de continuar, você sera enviado para a pagina de login"
+      );
+      navigate("/loginusuario");
     }
   };
 
   if (!festival) {
     return <div>Evento não encontrado.</div>;
   }
-  
+
+  function monthName(n) {
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    return months[n - 1];
+  }
+
   return (
     <div className="festival-details">
-      <h1>{festival.name}</h1>
-      <p>{festival.description}</p>
-      <p>Data: {festival.time}</p> 
-      <p>Local: {festival.place}</p>
-      <ul>
-        {festival.categories.map((categoria, index) => (
-          <li key={index}>{categoria.type}</li> 
-        ))}
-      </ul>
-      <button onClick={redirectToInscriptionPage} className="btn-inscription">Inscrever no Festival</button>
-      <button onClick={fetchSubscriptions}>Ver inscrições</button> 
-      {userString.roles && userString.roles.includes('ADMIN') && (
-        <button onClick={handleShowUploadForm}>Subir regras</button>
-      )}
-      {showUploadForm && <UploadForm id={id} />} 
-      {(userString.roles && (userString.roles.includes('ADMIN') || userString.roles.includes('MANAGER'))) && (
-          <button onClick={handleDownload}>Download regras</button>
-      )}
+      <div className="head">
+        <span className="date">
+          <em className="day">{moment(festival.time).format("DD")}</em>
+          <em className="month">
+            {monthName(moment(festival.time).format("MM"))}
+          </em>
+        </span>
+        <h1 className="name">{festival.name}</h1>
+      </div>
+      <div className="info">
+        <p>{festival.description}</p>
+        <p>Horário: {moment(festival.time).format("HH:mm")}</p>
+        <p>Local: {festival.place}</p>
+        <ul>
+          {festival.categories.map((categoria, index) => (
+            <li key={index}>{categoria.type}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="buttons">
+        <button onClick={redirectToInscriptionPage} className="btn-inscription">
+          Inscrever no Festival
+        </button>
+        <button onClick={fetchSubscriptions}>Ver inscrições</button>
+        {userString.roles && userString.roles.includes("ADMIN") && (
+          <button onClick={handleShowUploadForm}>Subir regras</button>
+        )}
+        {showUploadForm && <UploadForm id={id} />}
+        {userString.roles &&
+          (userString.roles.includes("ADMIN") ||
+            userString.roles.includes("MANAGER")) && (
+            <button onClick={handleDownload}>Download regras</button>
+          )}
+      </div>
 
       {subscriptions.length > 0 && (
         <div className="subscription-details">
-          {subscriptions.map(subscription => (
+          {subscriptions.map((subscription) => (
             <div key={subscription.id}>
               <form>
                 <h3>{subscription.name}</h3>
-                <p>{"Descrição: "+subscription.description}</p>
-                <p>{"ID da inscrição"+subscription.id}</p>
-                <p>{"Categories:"+subscription.category_id}</p>
-                <p>{"Horario: "+subscription.time}</p>
-                <p>{"Staff: "+Array.isArray(subscription.staff) ? subscription.staff.join(", ") : ''}</p>
+                <p>{"Descrição: " + subscription.description}</p>
+                <p>{"ID da inscrição" + subscription.id}</p>
+                <p>{"Categories:" + subscription.category_id}</p>
+                <p>{"Horario: " + subscription.time}</p>
+                <p>
+                  {"Staff: " + Array.isArray(subscription.staff)
+                    ? subscription.staff.join(", ")
+                    : ""}
+                </p>
               </form>
             </div>
           ))}
--- src/handlers/auth.handler.js
+++ src/handlers/auth.handler.js
@@ -1,5 +1,6 @@
 import { AuthServiceFactory } from "../services/auth.service";
 import { validarCPF, validarNumeroTelefone } from "../utils/validations";
+import moment from "moment";
 
 export async function loginHandler(e) {
   e.preventDefault();
@@ -14,7 +15,7 @@ export async function registerHandler(e) {
   const formData = new FormData(e.target);
   const registerData = Object.fromEntries(formData.entries());
   registerData.role_id = parseInt(registerData.role_id);
-  registerData.birth_date = new Date(registerData.birth_date + 'T00:00:00').toLocaleDateString('pt-BR', { timeZone: 'UTC' });
+  registerData.birth_date = moment(registerData.birth_date).format('dd/MM/yyyy');
 
   if (!validarCPF(registerData.cpf)) {
     alert('CPF inválido!');
