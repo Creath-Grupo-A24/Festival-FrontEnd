39acfee2d958720eb5b345a7e1f595d43141a928
gustavofg1pontes <gustavo.andre@aluno.ifsp.edu.br>
2024-02-18 21:51:13 -0300

    style: view company

--- src/app/company/view/view-company.page.js
+++ src/app/company/view/view-company.page.js
@@ -5,65 +5,77 @@ import { AuthServiceFactory } from "../../../services/auth.service";
 import ViewUser from "../../../components/user/view-user.component";
 
 const ViewCompany = ({ user, company }) => {
-    const [companyUsers, setCompanyUsers] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
+  const [companyUsers, setCompanyUsers] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        try {
-            if (company == null && user && user.roles && user.roles.includes("MANAGER")) {
-                navigate("/company/create");
+  useEffect(() => {
+    try {
+      if (
+        company == null &&
+        user &&
+        user.roles &&
+        user.roles.includes("MANAGER")
+      ) {
+        navigate("/company/create");
+      }
+      const fetchUsers = async () => {
+        if (!company) return;
+        const users = await AuthServiceFactory.create().getUsersByCompany(
+          company.id
+        );
+
+        const usersByRole = {};
+        users.forEach((user) => {
+          user.roles.forEach((role) => {
+            if (!usersByRole[role]) {
+              usersByRole[role] = [];
             }
-            const fetchUsers = async () => {
-                if (!company) return
-                const users = await AuthServiceFactory.create().getUsersByCompany(company.id)
+            usersByRole[role].push(user);
+          });
+        });
 
-                const usersByRole = {};
-                users.forEach((user) => {
-                    user.roles.forEach((role) => {
-                        if (!usersByRole[role]) {
-                            usersByRole[role] = [];
-                        }
-                        usersByRole[role].push(user);
-                    });
-                });
+        setCompanyUsers(usersByRole);
+        setLoading(false);
+      };
+      fetchUsers();
+    } catch (error) {
+      console.error("Erro ao buscar usuários da companhia:", error);
+    }
+  }, [company, navigate, user]);
 
-                setCompanyUsers(usersByRole);
-                setLoading(false);
-            }
-            fetchUsers()
-        } catch (error) {
-            console.error("Erro ao buscar usuários da companhia:", error);
-        }
-    }, [company, navigate, user])
-    
-    return (
-        <div>
-            <div className="users-container">
-                {loading ? (
-                    <div className="loading">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                ) : (
-                    Object.keys(companyUsers).sort((a, b) => {
-                        const roleOrder = ['MANAGER', 'TEACHER', 'DANCER'];
-                        return roleOrder.indexOf(a) - roleOrder.indexOf(b);
-                      }).map((role, index) => (
-                        <div key={index}>
-                            <h2>{role}</h2>
-                            <div>
-                                {companyUsers[role].map((user, userIndex) => (
-                                    <ViewUser key={userIndex} user={user} />
-                                ))}
-                            </div>
-                        </div>
-                    ))
-                )}
-            </div>
+  return (
+    <div className="users-container">
+      {loading ? (
+        <div className="loading">
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
-    );
+      ) : (
+        <>
+          <h1 className="company-name">{company.name}</h1>
+          <div className="users-by-roles">
+            {Object.keys(companyUsers)
+              .sort((a, b) => {
+                const roleOrder = ["MANAGER", "TEACHER", "DANCER"];
+                return roleOrder.indexOf(a) - roleOrder.indexOf(b);
+              })
+              .map((role, index) => (
+                <div key={index}>
+                  <h2 className="role-type">{role}</h2>
+                  <div className="role-container">
+                    {companyUsers[role].map((user, userIndex) => (
+                      <ViewUser className="user" key={userIndex} user={user} />
+                    ))}
+                  </div>
+                </div>
+              ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ViewCompany;
--- src/components/user/view-user.component.js
+++ src/components/user/view-user.component.js
@@ -4,29 +4,40 @@ import moment from "moment";
 import { FaUserCircle } from "react-icons/fa";
 
 const ViewUser = ({ user }) => {
-    const [expandedUser, setExpandedUser] = useState(false);
+  const [expandedUser, setExpandedUser] = useState(false);
 
-    const toggleDetails = () => {
-        setExpandedUser((prevUser) => !prevUser);
-    };
+  const toggleDetails = () => {
+    setExpandedUser((prevUser) => !prevUser);
+  };
 
-    return (
-        <div className="user" onClick={() => toggleDetails()}>
-            <FaUserCircle className="unique-user-icon"/><br/>
-            <span className="username">{user.username}</span><br/>
-            <span className="real-name">{user.name}</span><br/>
-            <span className="arrow">{expandedUser ? '▼' : '►'}</span>
+  return (
+    <div
+      className="user"
+      onClick={() => toggleDetails()}
+      style={{ height: expandedUser ? "120px" : "60px" }}
+    >
+      <FaUserCircle className="unique-user-icon" />
+      <span className="username">{user.username}</span>
+      <br />
+      <span className="real-name">{user.name}</span>
+      <br />
+      <span className="arrow">{expandedUser ? "▼" : "►"}</span>
 
-            {expandedUser && (
-                <div className="user_details">
-                    <span className="email">{user.email}</span><br/>
-                    <span className="cpf">{user.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')}</span><br/>
-                    <span className="birthdate">{moment(user.birth_date).format("DD/MM/yyyy")}</span>
-                </div>
-            )}
+      {expandedUser && (
+        <div className="user_details">
+          <span className="email">{user.email}</span>
+          <br />
+          <span className="cpf">
+            {user.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")}
+          </span>
+          <br />
+          <span className="birthdate">
+            {moment(user.birth_date).format("DD/MM/yyyy")}
+          </span>
         </div>
-
-    );
+      )}
+    </div>
+  );
 };
 
 export default ViewUser;
